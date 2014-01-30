960ffcd9a5bc15101e5bb8c693dda560cddd33fd
Shigglewitz <shigglewitz@gmail.com>
2014-01-30 15:38:28 +0800

    Added pixel shifting

--- graphing/src/test/java/org/dkeeney/graphing/ColorGrapherTest.java
+++ graphing/src/test/java/org/dkeeney/graphing/ColorGrapherTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import org.dkeeney.graphing.equations.exceptions.InvalidEquationException;
 import org.dkeeney.utils.ImageComparison;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ColorGrapherTest {
@@ -23,6 +24,13 @@ public class ColorGrapherTest {
     }
 
     @Test
+    public void testGraphZoom() throws InvalidEquationException, IOException {
+        this.testColorGraph("X^2+Y^2", "sin(Y*X)*128", "X", 2, 0, 200,
+                DIAGNOSTIC, null, true, "graph-5");
+    }
+
+    @Ignore
+    @Test
     public void testGraph() throws InvalidEquationException, IOException {
         this.testColorGraph("X^2+Y", "0", "0", null, true, "graph-4");
     }
@@ -30,10 +38,35 @@ public class ColorGrapherTest {
     public void testColorGraph(String redEq, String greenEq, String blueEq,
             String compareFile, boolean save, String fileName)
             throws InvalidEquationException, IOException {
+        this.testColorGraph(redEq, greenEq, blueEq, 1, compareFile, save,
+                fileName);
+    }
+
+    public void testColorGraph(String redEq, String greenEq, String blueEq,
+            int zoomFactor, String compareFile, boolean save, String fileName)
+            throws InvalidEquationException, IOException {
+        this.testColorGraph(redEq, greenEq, blueEq, zoomFactor, DIAGNOSTIC,
+                compareFile, save, fileName);
+    }
+
+    public void testColorGraph(String redEq, String greenEq, String blueEq,
+            int zoomFactor, boolean diagnostic, String compareFile,
+            boolean save, String fileName) throws InvalidEquationException,
+            IOException {
+        this.testColorGraph(redEq, greenEq, blueEq, zoomFactor, 0, 0,
+                diagnostic, compareFile, save, fileName);
+    }
+
+    public void testColorGraph(String redEq, String greenEq, String blueEq,
+            int zoomFactor, int xShift, int yShift, boolean diagnostic,
+            String compareFile, boolean save, String fileName)
+            throws InvalidEquationException, IOException {
         ColorGrapher cg = new ColorGrapher(redEq, greenEq, blueEq);
-        if (DIAGNOSTIC) {
+        if (diagnostic) {
             cg.enableDiagnostics();
         }
+        cg.setZoomFactor(zoomFactor);
+        cg.setPixelShift(xShift, yShift);
         BufferedImage experiment = cg.getGraph();
         if (compareFile != null) {
             ImageComparison.compareRandomPixels(compareFile, experiment);
