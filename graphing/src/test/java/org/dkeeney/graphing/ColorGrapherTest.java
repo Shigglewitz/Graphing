0be3a94467e1a2b96498d1d44abcea17aca539fa
Shigglewitz <shigglewitz@gmail.com>
2014-01-30 09:04:30 +0800

    Refactor color graph tests

--- graphing/src/test/java/org/dkeeney/graphing/ColorGrapherTest.java
+++ graphing/src/test/java/org/dkeeney/graphing/ColorGrapherTest.java
@@ -8,21 +8,39 @@ import org.dkeeney.utils.ImageComparison;
 import org.junit.Test;
 
 public class ColorGrapherTest {
+    private static final boolean DIAGNOSTIC = true;
+
     @Test
     public void testGetGraph() throws InvalidEquationException, IOException {
-        ColorGrapher cg = new ColorGrapher("X", "Y", "X+Y");
-        cg.enableDiagnostics();
-        BufferedImage experiment = cg.getGraph();
-        ImageComparison.compareRandomPixels("color-graph-1", experiment);
+        this.testColorGraph("X", "Y", "X+Y", "color-graph-1", false, "");
     }
 
     @Test
     public void testTrigonometricGraph() throws InvalidEquationException,
             IOException {
-        ColorGrapher cg = new ColorGrapher("X^2+Y^2", "sin(Y*X)*128", "X");
-        cg.enableDiagnostics();
+        this.testColorGraph("X^2+Y^2", "sin(Y*X)*128", "X",
+                "trigonometric-graph-1", false, "");
+    }
+
+    @Test
+    public void testGraph() throws InvalidEquationException, IOException {
+        this.testColorGraph("X^2+Y", "0", "0", null, true, "graph-4");
+    }
+
+    public void testColorGraph(String redEq, String greenEq, String blueEq,
+            String compareFile, boolean save, String fileName)
+            throws InvalidEquationException, IOException {
+        ColorGrapher cg = new ColorGrapher(redEq, greenEq, blueEq);
+        if (DIAGNOSTIC) {
+            cg.enableDiagnostics();
+        }
         BufferedImage experiment = cg.getGraph();
-        ImageComparison
-                .compareRandomPixels("trigonometric-graph-1", experiment);
+        if (compareFile != null) {
+            ImageComparison.compareRandomPixels(compareFile, experiment);
+        }
+        if (save) {
+            ImageMaker.saveImage(experiment, fileName,
+                    ImageMaker.DEFAULT_EXTENSION);
+        }
     }
 }
