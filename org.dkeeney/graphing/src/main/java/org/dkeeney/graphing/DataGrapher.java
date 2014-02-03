31db14c864e5b053d07a41316dc2c2158c42fd68
Shigglewitz <shigglewitz@gmail.com>
2014-02-03 17:25:50 +0800

    Handles multiple data sets at once

--- org.dkeeney/graphing/src/main/java/org/dkeeney/graphing/DataGrapher.java
+++ org.dkeeney/graphing/src/main/java/org/dkeeney/graphing/DataGrapher.java
@@ -15,8 +15,10 @@ import org.dkeeney.utils.ImageMaker;
 public class DataGrapher {
     public static final int DEFAULT_WIDTH = 400;
     public static final int DEFAULT_HEIGHT = DEFAULT_WIDTH;
+    private static final Color DEFAULT_GRAPH_COLOR = Color.BLACK;
 
-    private final List<Point> data;
+    private final List<List<Point>> data;
+    private final List<Color> graphColors;
     private double minX;
     private double maxX;
     private double minY;
@@ -25,31 +27,49 @@ public class DataGrapher {
     private Color graphBackground = Color.WHITE;
     private Color gridColor = Color.GRAY;
     private final Color axisColor = Color.BLACK;
-    private final Color graphColor = Color.RED;
     private final boolean drawGrid = false;
     private final int pixelsPerTick = 3;
     private final int unitsBetweenTicks = 1;
     private final boolean drawTicks = true;
 
-    public DataGrapher(List<Point> data) {
-        this(data.toArray(new Point[data.size()]));
+    public DataGrapher() {
+        this.data = new ArrayList<List<Point>>();
+        this.graphColors = new ArrayList<>();
     }
 
-    public DataGrapher(Point[] data) {
-        this.data = new ArrayList<>();
-        this.minX = data[0].getX();
-        this.maxX = data[0].getX();
-        this.minY = data[0].getY();
-        this.maxY = data[0].getY();
+    public void addPoints(List<Point> points) {
+        this.addPoints(points.toArray(new Point[points.size()]));
+    }
 
-        for (Point p : data) {
-            this.addPoint(p);
+    public void addPoints(List<Point> points, Color color) {
+        this.addPoints(points.toArray(new Point[points.size()]), color);
+    }
+
+    public void addPoints(Point[] points) {
+        this.addPoints(points, DEFAULT_GRAPH_COLOR);
+    }
+
+    public void addPoints(Point[] points, Color color) {
+        List<Point> pointList = new ArrayList<>();
+        synchronized (this.data) {
+            if (this.data.size() == 0) {
+                this.minX = points[0].getX();
+                this.maxX = points[0].getX();
+                this.minY = points[0].getY();
+                this.maxY = points[0].getY();
+            }
         }
 
-        Collections.sort(this.data, new PointCompare());
+        for (Point p : points) {
+            this.addPoint(p, pointList);
+        }
+
+        Collections.sort(pointList, new PointCompare());
+        this.data.add(pointList);
+        this.graphColors.add(color);
     }
 
-    private void addPoint(Point p) {
+    private void addPoint(Point p, List<Point> points) {
         if (p.getX() > this.maxX) {
             this.maxX = p.getX();
         }
@@ -62,7 +82,7 @@ public class DataGrapher {
         if (p.getY() < this.minY) {
             this.minY = p.getY();
         }
-        this.data.add(p);
+        points.add(p);
     }
 
     public BufferedImage getGraph() {
@@ -84,16 +104,20 @@ public class DataGrapher {
                 this.axisColor, this.unitsBetweenTicks, this.pixelsPerTick);
 
         // draw graphs
-        Point p = this.data.get(0);
-        graphics.setColor(this.graphColor);
-        for (int i = 1; i < this.data.size(); i++) {
-            graphics.drawLine(
-                    GraphUtils.getXpixel(width, minX, maxX, p.getX()),
-                    GraphUtils.getYpixel(height, minY, maxY, p.getY()),
-                    GraphUtils.getXpixel(width, minX, maxX, this.data.get(i)
-                            .getX()), GraphUtils.getYpixel(height, minY, maxY,
-                            this.data.get(i).getY()));
-            p = this.data.get(i);
+        Point p;
+        List<Point> l;
+        for (int i = 0; i < this.data.size(); i++) {
+            l = this.data.get(i);
+            p = l.get(0);
+            graphics.setColor(this.graphColors.get(i));
+            for (int j = 1; j < l.size(); j++) {
+                graphics.drawLine(GraphUtils.getXpixel(width, minX, maxX,
+                        p.getX()), GraphUtils.getYpixel(height, minY, maxY,
+                        p.getY()), GraphUtils.getXpixel(width, minX, maxX, l
+                        .get(j).getX()), GraphUtils.getYpixel(height, minY,
+                        maxY, l.get(j).getY()));
+                p = l.get(j);
+            }
         }
 
         graphics.dispose();
